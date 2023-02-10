fe6cae648cadbb28d3c1cc5e2bece0d0e0a8e17e
vaitkeviciut <indre.vaitkeviciute96@gmail.com>
2023-02-10 13:35:50 +0200

    commit1

---
+++ script.js
@@ -0,0 +1,32 @@
+let activityForm = document.querySelector('#activity-form');
+let activitySelection = document.querySelector('#category');
+let activityOutput = document.querySelector('#activity-output');
+
+activityForm.after(activityOutput);
+
+let activityArr = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"];
+
+activityArr.map(option => {
+    let activityOption = document.createElement('option');
+    activityOption.value = option
+    activityOption.textContent = option[0].toUpperCase() + option.slice(1);
+
+    activitySelection.append(activityOption)
+})
+
+activityForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let selectedCategory = activitySelection.value;
+
+    fetch(`http://www.boredapi.com/api/activity?type=${selectedCategory}`)
+        .then(res => res.json())
+        .then(data => {
+            let activityType = data.type
+            let action = data.activity
+            console.log(activityType)
+            console.log(action)
+
+            activityOutput.textContent = `${action}`
+        })
+})
